d684cf02062c31808e8b7a861cbb5a99f719d1be
seeker1911 <michaelmead007@icloud.com>
2020-03-09 10:02:01 -0500

    add a game

--- games/number_guessing/guess_one.py
+++ games/number_guessing/guess_one.py
@@ -1,8 +1,33 @@
-secret_number = 7
+import random
 
-guess = input("What number am I thinking of? ")
+playing = True
 
-if secret_number == guess:
-    print("Yay! You got it.")
-else:
-    print("No, that's not it.")
+while playing:
+    secret_number = random.randint(0, 100)
+
+    guesses_left = 5
+    guessed_correctly = False
+
+    while not guessed_correctly:
+        if guesses_left > 0:
+            guess = input("What number am I thinking of? ")
+            guesses_left -= 1
+
+            if secret_number == int(guess):
+                print("Yay! You got it.")
+                guessed_correctly = True
+            elif secret_number > int(guess):
+                print("Think bigger.")
+            else:
+                print("Think smaller.")
+        else:
+            print("You lose! the number was ", secret_number)
+            break
+
+    print("Good job! you played a game.")
+
+    play_again = input("Would you like to play again? (y or n)")
+    if play_again.lower().startswith('y'):
+        playing = True
+    else:
+        playing = False
